bc77e84cf5f1c6efd65ab1d298c0a65facb30d41
kamlesh <k24suthar@gmail.com>
2024-07-18 23:53:09 +0530

    view all jobs component : adding a file that was missed in the previous commit.

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import Navbar from "./components/Navbar";
 import Hero from "./components/Hero";
 import HomeCards from "./components/HomeCards"
 import JobListings from "./components/JobListings";
+import ViewAllJobs from "./components/ViewAllJobs";
 
 function App() {
 
@@ -9,26 +10,12 @@ function App() {
     <>
 
       <Navbar />
-
-      <Hero title="Test title" subTitle="This is Testing Subtitle...." />
-
-      {/* <!-- Developers and Employers --> */}
+      <Hero />
       <HomeCards />
-
-      {/* <!-- Browse Jobs --> */}
       <JobListings />
-
-      <section className="m-auto max-w-lg my-10 px-6">
-        <a
-          href="jobs.html"
-          className="block bg-black text-white text-center py-4 px-6 rounded-xl hover:bg-gray-700"
-        >View All Jobs</a
-        >
-      </section>
-
+      <ViewAllJobs />
 
     </>
-
   )
 }
 
