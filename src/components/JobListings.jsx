887cabc9b12891abcd0821b3ca670511e8acb979
kamlesh <k24suthar@gmail.com>
2024-07-20 23:40:00 +0530

    proxy config, handling fetch error

--- src/components/JobListings.jsx
+++ src/components/JobListings.jsx
@@ -6,16 +6,21 @@ const delay = (ms) => new Promise(resolve => setTimeout(resolve, ms));
 const JobListings = ({ isHome = true }) => {
     const [isLoading, setLoading] = useState(true)
     const [recent, setRecent] = useState([])
-    
+    const [error, setError] = useState(null);
+
     const fetchData = async () => {
         await delay(1000);
         try {
-            const response = await fetch('http://localhost:4000/jobs')
+            const url = isHome ? '/api' : '/api?_limit=10'
+            const response = await fetch(url)
+            if(!response.ok){
+                throw new Error('Failed to fetch data')
+            }
             const result = await response.json()
-            const recentJobs = isHome ? result.slice(0, 3) : result;
-            setRecent(recentJobs);
+            setRecent(result);
         } catch (error) {
             console.error(error)
+            setError('Failed to fetch data. Please try again later.')
         } finally {
             setLoading(false)
         }
@@ -29,7 +34,11 @@ const JobListings = ({ isHome = true }) => {
         <>
             {isLoading ? (
                 <Spinner />
-            ) : (
+            ) : error ? (
+                <>
+                <div>{error}</div>
+                </>
+            ): (
                 <section className="bg-blue-50 px-4 py-10">
                     <div className="container-xl lg:container m-auto">
                         <h2 className="text-3xl font-bold text-indigo-500 mb-6 text-center">
