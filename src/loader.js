06354a666eb5e2fb0398920648710b8d6848665d
kamlesh <k24suthar@gmail.com>
2024-07-21 18:21:03 +0530

    loader to getJobs

---
+++ src/loader.js
@@ -0,0 +1,12 @@
+import getJobs from './api'
+export async function loader({ params }) {
+
+    const jobs = await getJobs(params.jobId);
+    if (!jobs) {
+        throw new Response("", {
+            status: 404,
+            statusText: "Not Found",
+        })
+    }
+    return jobs;
+}
