89cf663ac5d995e6e31ab43dfb679287d25689be
kamlesh <k24suthar@gmail.com>
2024-08-01 17:05:21 +0530

    minor changes

--- src/api.js
+++ src/api.js
@@ -1,7 +1,7 @@
 export default async function getJobs(jobId) {
 
     try {
-        const url = `/api?id=${jobId}`
+        const url = `/api/${jobId}`
         const response = await fetch(url)
         if (!response.ok) {
             throw new Error('Failed to fetch data')
@@ -10,8 +10,7 @@ export default async function getJobs(jobId) {
         if (result.length === 0) {
             throw new Error('No jobs found'); 
         }
-        // console.log(result[0])
-        return result[0]
+        return result
     } catch (error) {
         console.error('Error in getJobs:', error)
         throw error;
