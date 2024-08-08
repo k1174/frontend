ff49034261c8b9f4d9932c349ef9ab31a9fb729d
kamlesh <k24suthar@gmail.com>
2024-08-08 15:16:31 +0530

    added auth

---
+++ src/pages/auth/Profile.jsx
@@ -0,0 +1,56 @@
+import { useEffect, useState } from 'react';
+import { useNavigate } from 'react-router-dom';
+import { getUserProfile } from '../../api';
+
+export default function Profile() {
+    const [loading, setLoading] = useState(true);
+    const [user, setUser] = useState(null);
+    const navigate = useNavigate();
+
+    const token = localStorage.getItem('token');
+
+    useEffect(() => {
+        const fetchUser = async () => {
+
+            if (!token) {
+                navigate('/login');
+            }
+            else{
+                // Fetch user profile data if token exists
+                try{
+                    const userProfile = await getUserProfile(token)
+                    setUser(userProfile) 
+                    setLoading(false)
+                }
+                catch(error){
+                    console.error('Failed to fetch user profile: '. error)
+                }
+            }
+        }
+
+        fetchUser()
+    }, [token, navigate])
+
+    if (loading) {
+        return (
+            <div>
+                <h1>Loading...</h1>
+            </div>
+        )
+    }
+
+    if (!user) {
+        return <div>Unable to fetch user data. Please try again later.</div>;
+    }
+
+    return (
+        <div>
+            <h1>Profile</h1>
+            <div>
+                <p>Name: {user.name}</p>
+                <p>Email: {user.email}</p>
+                <p>Role: {user.role}</p>
+            </div>
+        </div>
+    )
+}
---
+++ src/pages/auth/Register.jsx
@@ -0,0 +1,125 @@
+import { useNavigate, Link } from "react-router-dom"
+
+export default function Register() {
+
+    const navigate = useNavigate();
+    const handleSubmit = async (e) => {
+        e.preventDefault()
+        try {
+
+            const formData = new FormData(e.target)
+            const data = Object.fromEntries(formData.entries())
+
+            console.log('Submitting data:', data);
+            const response = await fetch('/auth/register', {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json',
+                },
+                body: JSON.stringify(data)
+            })
+
+
+            if (!response.ok) {
+                const errorData = await response.json();
+                const errorMessage = errorData.error || 'Registration failed';
+                throw new Error(errorMessage);
+            }
+            
+            console.log("Registration Successful");
+            navigate('/login');
+        }
+        catch (error) {
+            console.error('Registration Failed:', error.message);
+            // Optionally, display the error message to the user
+            alert('Registration Failed: ' + error.message);
+        }
+    }
+
+
+
+    return (
+        <>
+            <div className="flex min-h-full flex-1 flex-col justify-center px-6 py-12 lg:px-8">
+                <div className="sm:mx-auto sm:w-full sm:max-w-sm">
+                    <h2 className="mt-10 text-center text-2xl font-bold leading-9 tracking-tight text-gray-900">
+                        Regiser your account
+                    </h2>
+                </div>
+
+                <div className="mt-10 sm:mx-auto sm:w-full sm:max-w-sm">
+                    <form onSubmit={handleSubmit} className="space-y-6">
+                        <div>
+                            <label htmlFor="name" className="block text-sm font-medium leading-6 text-gray-900">
+                                Name
+                            </label>
+                            <div className="mt-2">
+                                <input
+                                    id="name"
+                                    name="name"
+                                    type="name"
+                                    required
+                                    placeholder=" Enter Your Name"
+                                    className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                                />
+                            </div>
+                        </div>
+
+                        <div>
+                            <label htmlFor="email" className="block text-sm font-medium leading-6 text-gray-900">
+                                Email address
+                            </label>
+                            <div className="mt-2">
+                                <input
+                                    id="email"
+                                    name="email"
+                                    type="email"
+                                    required
+                                    autoComplete="email"
+                                    className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                                />
+                            </div>
+                        </div>
+
+                        <div>
+                            <div className="flex items-center justify-between">
+                                <label htmlFor="password" className="block text-sm font-medium leading-6 text-gray-900">
+                                    Create Password
+                                </label>
+
+                            </div>
+                            <div className="mt-2">
+                                <input
+                                    id="password"
+                                    name="password"
+                                    type="password"
+                                    required
+                                    autoComplete="current-password"
+                                    className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                                />
+                            </div>
+                        </div>
+
+                        <div>
+                            <button
+                                type="submit"
+                                className="flex w-full justify-center rounded-md bg-indigo-600 px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
+                            >
+                                Register
+                            </button>
+                        </div>
+                    </form>
+
+
+                    <p className="mt-10 text-center text-sm text-gray-500">
+                        Already a member?{' '}
+                        <Link to="/login" className="font-semibold text-indigo-600 hover:text-indigo-500">
+                            Login
+                        </Link>    
+                    </p>
+
+                </div>
+            </div>
+        </>
+    )
+}
