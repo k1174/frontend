df207aa1476bc087264683fbe23bc615cd1d87e4
kamlesh <k24suthar@gmail.com>
2024-08-09 18:17:14 +0530

    added

---
+++ src/components/Test.jsx
@@ -0,0 +1,17 @@
+export default function Test() {
+    return (
+        <div>
+            <h1>Test</h1>
+            <p>This is a test page</p>
+            <div className="bg-contain bg-no-repeat  w-96 bg-[url('https://media1.thrillophilia.com/filestore/40zd6zg27zprhg4cin0ig2kei9f3_16112688_680133948824405_5215334608666219017_o.jpg')]">
+                <h1>Test</h1>
+                <p>This is a test page</p>
+                ldjdsnfkjndfkjn
+                <h1>Test</h1>
+                <p>This is a test page</p>
+                ldjdsnfkjndfkjn
+            </div>
+        </div>
+    )
+}
+
