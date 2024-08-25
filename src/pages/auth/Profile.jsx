28db0d460b339d4c886d111123df6228c96c930f
kamlesh <k24suthar@gmail.com>
2024-08-25 16:55:11 +0530

    Add sections for registered and created events in Profile component

--- src/pages/auth/Profile.jsx
+++ src/pages/auth/Profile.jsx
@@ -1,11 +1,15 @@
 import { useEffect, useState } from 'react';
 import { useNavigate } from 'react-router-dom';
-import { getUserProfile } from '../../api';
+import { getUserProfile, getUserRegistrations } from '../../api';
+import JobListing from '../../components/JobListing';
+import { useAuth } from '../../../context/AuthContext'
 
 export default function Profile() {
     const [loading, setLoading] = useState(true);
     const [user, setUser] = useState(null);
+    const [registrations, setRegistrations] = useState(null);
     const navigate = useNavigate();
+    const { isAdmin } = useAuth();
 
     const token = localStorage.getItem('token');
 
@@ -19,9 +23,10 @@ export default function Profile() {
                 // Fetch user profile data if token exists
                 try {
                     const userProfile = await getUserProfile(token)
+                    const userRegistration = await getUserRegistrations(token)
                     setUser(userProfile)
                     setLoading(false)
-
+                    setRegistrations(userRegistration)
                 }
                 catch (error) {
                     console.error('Failed to fetch user profile: '.error)
@@ -40,9 +45,6 @@ export default function Profile() {
         )
     }
 
-    if (!user) {
-        return <div>Unable to fetch user data. Please try again later.</div>;
-    }
 
     return (
         <>
@@ -52,16 +54,55 @@ export default function Profile() {
                     <p>Name: {user.name}</p>
                     <p>Email: {user.email}</p>
                     <p>Role: {user.role}</p>
+
                     {/* List of registered events */}
-                    <h2>Registered Events</h2>
-                    {/* <ul>
-                    {user.events.map((event) => (
-                        <li key={event.id}>
-                        <p>{event.name}</p>
-                        <p>{event.date}</p>
-                        </li>
-                        ))}
-                        </ul> */}
+                    {!isAdmin &&
+                        <>
+                            <div>
+
+                                <h2>Registered Events</h2>
+                                <section className="bg-blue-50 px-4 py-10">
+                                    <div className="container-xl lg:container m-auto">
+                                        <h2 className="text-3xl font-bold text-indigo-500 mb-6 text-center">
+                                            {"Registered Events"}
+                                        </h2>
+                                        <div className="grid grid-cols-1 md:grid-cols-3 gap-6">
+                                            {registrations && registrations.length > 0 ? (
+                                                registrations.map((event) => (
+                                                    <JobListing key={event._id} job={event} />
+                                                ))
+                                            ) : (
+                                                <p>No events found.</p>
+                                            )}
+                                        </div>
+                                    </div>
+                                </section>
+                            </div>
+
+                            <div>
+                                <section className="bg-blue-50 px-4 py-10">
+                                    <div className="container-xl lg:container m-auto">
+                                        <h2 className="text-3xl font-bold text-indigo-500 mb-6 text-center">
+                                            {"Created Events"}
+                                        </h2>
+                                        <div className="grid grid-cols-1 md:grid-cols-3 gap-6">
+                                            {registrations && registrations.length > 0 ? (
+                                                registrations.map((event) => (
+                                                    <JobListing key={event._id} job={event} />
+                                                ))
+                                            ) : (
+                                                <p>No events found.</p>
+                                            )}
+                                        </div>
+                                    </div>
+                                </section>
+                            </div>
+                        </>
+
+
+                    }
+
+
                 </div>
             </div>
         </>
