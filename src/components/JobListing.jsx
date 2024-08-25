8c6be5a1cd527f8170ccb97a93aaf3cd9b475886
kamlesh <k24suthar@gmail.com>
2024-08-25 17:14:00 +0530

    Refactor JobListing component: remove description toggle and update URL logic

--- src/components/JobListing.jsx
+++ src/components/JobListing.jsx
@@ -1,36 +1,32 @@
-import { useState } from "react";
 import { FaMapMarkerAlt } from "react-icons/fa";
 import { Link } from "react-router-dom";
 const JobListing = ({ job, isHome, isAdmin }) => { // {job} -> Destructuring assignment
     const description = job.description.substring(0, 90) + '...';
-    const [showFullDesc, setShowFullDesc] = useState(false)
-    let url = isHome ? `/eventspage/${job._id}`: `${job._id}`
-    if(isAdmin){
-        url = isHome ? `/admin/${job._id}` : `${job._id}`
+
+    let url = isHome ? `/eventspage/${job._id}` : `${job._id}`
+    if (isAdmin) {
+        url = `/admin/${job._id}`
     }
     return (
         <>
-            <div className="bg-white rounded-xl shadow-md relative">
-                <div className="p-4">
+            <div className="bg-white rounded-xl shadow-md relative  hover:shadow-lg hover:ring-1 hover:ring-gray-200  ">
+                <div className="p-4 ">
                     <div className="mb-6">
                         <div className="text-gray-600 my-2">{job.type}</div>
                         <h3 className="text-xl font-bold"> {job.name} </h3>
                     </div>
 
-                    <div className="mb-4">{showFullDesc ? job.description : description}</div>
-                    
-                    <button onClick={() => (setShowFullDesc( prevState => (!prevState)))}
-                        className="mb-7 text-indigo-600">{showFullDesc ? "Less" : "More"}</button>
-                    <p className="mb-1">{job.time}</p>
-                    <p className="mb-4">{job.date.split('T')[0]}</p>
+                    <div className="mb-4 h-20 ">{description}</div>
 
-                    <h3 className="text-indigo-500 mb-2">Price: {job.price}</h3>
+
+                    <h3 className="text-indigo-500 mb-4">Price: {job.price}</h3>
+                    <p className="mb-2"> {new Date(job.date).toLocaleDateString()}   {job.time} <span></span></p>
 
                     <div className="border border-gray-100 mb-5"></div>
 
                     <div className="flex flex-col lg:flex-row justify-between mb-4">
                         <div className="text-orange-700 mb-3 flex items-start gap-x-1">
-                            <FaMapMarkerAlt className=" text-lg "/>
+                            <FaMapMarkerAlt className=" text-lg " />
                             {job.location}
                         </div>
                         <Link
