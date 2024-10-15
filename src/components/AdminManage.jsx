87b3ede7dc81da8c2af54366c278b6c867dc8ec9
kamlesh <k24suthar@gmail.com>
2024-10-15 16:52:01 +0530

    brochure and activity report

--- src/components/AdminManage.jsx
+++ src/components/AdminManage.jsx
@@ -1,5 +1,7 @@
 import { Form } from "react-router-dom";
 import AddImages from "./AddImages";
+import AddBrochure from "./AddBrochure";
+import AddActivityReport from "./AddActivityReport";
 export default function AdminManage() {
     return (
         <>
@@ -27,7 +29,10 @@ export default function AdminManage() {
                         Delete
                     </button>
                 </Form>
+                <hr className="my-3" />
+                <AddBrochure />
                 <AddImages />
+                <AddActivityReport />
             </div>
         </>
     )
