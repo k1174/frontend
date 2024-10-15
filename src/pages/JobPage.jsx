1fbb767b1f89ba5054432bedeb02412b924e649d
kamlesh <k24suthar@gmail.com>
2024-10-15 17:24:31 +0530

    remove feedback download

--- src/pages/JobPage.jsx
+++ src/pages/JobPage.jsx
@@ -5,7 +5,7 @@ import BackLink from "../components/BackLink"
 // import DownloadEmailsButton from "../components/Email";
 import { useAuth } from "../../context/AuthContext";
 import TotalRegistration from "../components/TotalRegistration";
-import DownloadReportButton from "../components/Report";
+// import DownloadReportButton from "../components/Report";
 // import Feedbacks from "../components/Feedbacks.jsx";
 const JobPage = () => {
     const job = useLoaderData();
@@ -55,7 +55,7 @@ const JobPage = () => {
                 <>
                     <div className="flex justify-center">
                         {/* <DownloadEmailsButton job={job} /> */}
-                        <DownloadReportButton job={job} />
+                        {/* <DownloadReportButton job={job} /> */}
                         <a href={url} className="text-blue-700 hover:text-white border border-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center me-2 mb-2" >DashBoard</a>
                         {/* <Feedbacks job={job} /> */}
                     </div>
