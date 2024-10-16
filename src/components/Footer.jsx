a234d4843a708810fad8cc6c8728a4d93773c4a7
Baljit Singh <thebaljitsinghin@gmail.com>
2024-10-17 00:42:04 +0530

    Add Feature: fixed the alignment of footer like flexing

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -2,22 +2,22 @@ const Footer = () => {
     return (
 
 
-        <footer className="bg-white rounded-lg shadow m-4">
-            <div className="w-full mx-auto max-w-screen-xl p-4 md:flex md:items-center md:justify-between">
+        <footer className="bg-white rounded-lg shadow mt-4 w-full">
+            <div className="w-full mx-auto max-w-screen-xl p-4 md:flex md:items-center md:justify-between  ">
                 <span className="text-sm text-gray-500 sm:text-center ">© 2024 <a href="" className="hover:underline">Events™</a>. All Rights Reserved.
                 </span>
-                <ul className="flex flex-wrap items-center mt-3 text-sm font-medium text-gray-500  sm:mt-0">
-                    <li>
+                <ul className="flex flex-wrap items-center mt-3  text-sm font-medium text-gray-500  sm:mt-0">
+                    {/* <li>
                         <a href="/" className="hover:text-blue-500 hover:underline me-4 md:me-6">Home</a>
+                    </li> */}
+                    <li>
+                        <a href="/past" className="hover:text-blue-500 hover:underline me-4 md:me-6">About Us</a>
                     </li>
                     <li>
-                        <a href="/past" className="hover:text-blue-500 hover:underline me-4 md:me-6">Past Events</a>
+                        <a href="/eventspage" className="hover:text-blue-500 hover:underline me-4 md:me-6">Contact Us</a>
                     </li>
                     <li>
-                        <a href="/eventspage" className="hover:text-blue-500 hover:underline me-4 md:me-6">Upcoming Events</a>
-                    </li>
-                    <li>
-                        <a href="/addjob" className="hover:text-blue-500 hover:underline">Add Events</a>
+                        <a href="https://www.instagram.com/it.at.pu/" className="hover:text-blue-500 hover:underline">IT@Insta</a>
                     </li>
                 </ul>
             </div>
