892f5ad228446dd2b2f7cd451e41afe0cb8626d2
Baljit Singh <thebaljitsinghin@gmail.com>
2024-10-17 01:27:45 +0530

    removed space in footer.jsx

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,8 +1,6 @@
 const Footer = () => {
     return (
 
-        // <div className="relative w-screen">
-
         
         <footer className="bg-white rounded-lg shadow m-4 ">
             <div className="w-full mx-auto max-w-screen-xl p-4 md:flex md:items-center md:justify-between  ">
