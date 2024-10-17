d161a5e65b8937d3d7530b2a6be8e854a2fe062d
Baljit Singh <thebaljitsinghin@gmail.com>
2024-10-17 13:46:29 +0530

    added the navbar.jsx

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -4,7 +4,7 @@ import { NavLink } from 'react-router-dom';
 import { useAuth } from '../../context/AuthContext';
 import logo from '../assets/images/image.png';
 import { IoMdArrowDropdown } from "react-icons/io";
-import { FaUserCircle } from 'react-icons/fa'; 
+import { FaUserCircle } from 'react-icons/fa';
 import { toast, ToastContainer } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
 
@@ -44,24 +44,16 @@ export default function Navbar() {
 
     const handleLogout = async () => {
         try {
-            // Remove the token from localStorage
             localStorage.removeItem('token');
-    
-            // Remove the cookie (you might need to do this via backend call if the cookie is httpOnly)
-    
-     
-        
-    
-            // Refresh the page after the toast
             setTimeout(() => {
-                window.location.reload();  // This will refresh the page
-            }, 2000);  // Refresh after 2 seconds, after the toast notification
+                window.location.reload();
+            }, 1000);
         } catch (error) {
             console.error('Error during logout: ', error);
             toast.error('Error logging out, please try again.');
         }
     };
-    
+
 
     const linkClass = ({ isActive }) =>
         `text-white hover:bg-gray-800 hover:text-white rounded-md px-4 py-2 transition ${isActive ? 'bg-gray-900' : ''}`;
