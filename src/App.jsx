dcfa4daf31117f78d3cd6a06d56089943207292d
kamlesh <k24suthar@gmail.com>
2024-10-18 12:07:01 +0530

    forgot passowrd and reset password

--- src/App.jsx
+++ src/App.jsx
@@ -30,6 +30,9 @@ import Table from './pages/Table.jsx';
 import AdminRoutes from './components/AdminRoutes.jsx';
 import Download from './components/Download.jsx';
 import DateRangeSelector from './components/DateRangeSelector.jsx';
+import RestPassword from './pages/auth/RestPassword.jsx';
+import ForgotPassword from './pages/auth/ForgotPassword.jsx';
+// import Feedbacks from './components/Feedbacks.jsx';
 
 const router = createBrowserRouter(
   createRoutesFromElements(
@@ -76,7 +79,10 @@ const router = createBrowserRouter(
 
       <Route path='/Register' element={<Register />} />
       <Route path='/Login' element={<Login />} />
+      <Route path='/forgotpassword/' element={<ForgotPassword />} />
+      <Route path='/password-reset/:token' element={<RestPassword />} />
       <Route path='/profile' element={<Profile />} />
+      {/* <Route path='/feedback/:jobId' element={<Feedbacks />} /> */}
 
       <Route path='*' element={<NotFound />}></Route>
     </Route>
---
+++ src/pages/auth/ForgotPassword.jsx
@@ -0,0 +1,87 @@
+import { useState } from "react";
+import { toast } from "react-toastify";
+import Spinner from "../../components/Spinner";
+
+const ForgotPassword = () => {
+    const [loading, setLoading] = useState(false);
+    
+    const handleSubmit = async (e) => {
+        e.preventDefault();
+        setLoading(true);
+        try {
+            const formData = new FormData(e.target);
+            const data = Object.fromEntries(formData.entries());
+
+            const response = await fetch('/auth/forgotpassword', { // Updated endpoint
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json',
+                },
+                body: JSON.stringify(data),
+            });
+
+            if (!response.ok) {
+                throw new Error('Sending reset link failed');
+            }
+            setLoading(false);
+            console.log("Reset link sent successfully");
+            toast.success("Please check your email for reset link");
+
+        } catch (error) {
+            setLoading(false);
+            console.error('Sending reset link failed:', error.message);
+            toast.error(`Sending reset link failed: ${error.message}`);
+        }
+    };
+
+
+    return (
+        <>
+            {loading &&
+                <div className="absolute inset-0 flex items-center justify-center z-20 backdrop-blur-sm">
+                    <Spinner />
+                </div>
+            }
+            <div className="flex min-h-full flex-1 flex-col justify-center px-6 py-12 lg:px-8">
+                <div className="sm:mx-auto sm:w-full sm:max-w-sm">
+                    <h2 className="mt-10 text-center text-2xl font-bold leading-9 tracking-tight text-gray-900">
+                        Sign in to your account
+                    </h2>
+                </div>
+
+                <div className="mt-10 sm:mx-auto sm:w-full sm:max-w-sm">
+                    <form onSubmit={handleSubmit} className="space-y-6">
+                        <div>
+                            <label htmlFor="email" className="block text-sm font-medium leading-6 text-gray-900">
+                                Email address
+                            </label>
+                            <div className="mt-2">
+                                <input
+                                    id="email"
+                                    name="email"
+                                    type="email"
+                                    required
+                                    autoComplete="email"
+                                    pattern="/^([A-Z0-9_+-]+\.?)*[A-Z0-9_+-]@([A-Z0-9][A-Z0-9-]*\.)+[A-Z]{2,}$/i"
+                                    className="block w-full rounded-md border-0 pl-1 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6 focus:outline-none"
+                                />
+                            </div>
+                        </div>
+
+
+                        <div>
+                            <button
+                                type="submit"
+                                className="flex w-full justify-center rounded-md bg-indigo-600 px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
+                            >
+                                Send Reset Link
+                            </button>
+                        </div>
+                    </form>
+                </div>
+            </div>
+        </>
+    )
+}
+
+export default ForgotPassword
