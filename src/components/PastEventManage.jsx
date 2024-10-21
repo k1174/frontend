592b2a4313af1e25b0f30e6f57a18857b1e9ba35
kamlesh <k24suthar@gmail.com>
2024-10-21 23:55:20 +0530

    show if something is not uploaded

--- src/components/PastEventManage.jsx
+++ src/components/PastEventManage.jsx
@@ -4,28 +4,36 @@ const PastEventManage = ({ job }) => {
 
     return (
         <>
-            {(hasBrochure || hasActivityReport) &&
-                <div className="bg-white p-6 rounded-lg shadow-md mt-6 ">
 
-                    <h3 className="text-xl font-bold mb-6">Explore</h3>
+            <div className="bg-white p-6 rounded-lg shadow-md mt-6 ">
 
-                    {hasBrochure &&
-                        <a href={`${job.brochure}`}
-                            className="bg-indigo-500 hover:bg-indigo-600 text-white text-center font-bold py-2 px-4 rounded-full w-full focus:outline-none focus:shadow-outline mt-4 block"
-                        >
-                            Brochure
-                        </a>
-                    }
-                    {hasActivityReport &&
-                        <a href={`${job.ActivityReport}`}
-                            className="bg-indigo-500 hover:bg-indigo-600 text-white text-center font-bold py-2 px-4 rounded-full w-full focus:outline-none focus:shadow-outline mt-4 block"
-                        >
-                            Activity Report
-                        </a>
-                    }
+                <h3 className="text-xl font-bold mb-6">Explore</h3>
 
-                </div>
-            }
+                {hasBrochure ?
+                    <a href={`${job.brochure}`}
+                        className="bg-indigo-500 hover:bg-indigo-600 text-white text-center font-bold py-2 px-4 rounded-full w-full focus:outline-none focus:shadow-outline mt-4 block"
+                    >
+                        Brochure
+                    </a>
+                    :
+                    <p className="bg-indigo-500 hover:bg-indigo-600 text-white text-center font-bold py-2 px-4 rounded-full w-full focus:outline-none focus:shadow-outline mt-4 block" >
+                        Brochure not available
+                    </p>
+                }
+                {hasActivityReport ?
+                    <a href={`${job.ActivityReport}`}
+                        className="bg-indigo-500 hover:bg-indigo-600 text-white text-center font-bold py-2 px-4 rounded-full w-full focus:outline-none focus:shadow-outline mt-4 block"
+                    >
+                        Activity Report
+                    </a>
+                    :
+                    <p className="bg-indigo-500 hover:bg-indigo-600 text-white text-center font-bold py-2 px-4 rounded-full w-full focus:outline-none focus:shadow-outline mt-4 block" >
+                        Activity Report not available
+                    </p>
+                }
+
+            </div>
+
         </>
     )
 }
