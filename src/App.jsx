9451fde7ce39b7cbc043779231959d320fdf03d5
Baljit Singh <baljitsingh692002@gmail.com>
2024-10-21 13:36:48 +0530

    Landing Page: changed the ui/ux of page (#6)
    * Landing Page: changed the ui/ux of page
    
    * formatted the document
    
    * changed  homepage image
    
    * formatted the document
    
    * updated

--- src/App.jsx
+++ src/App.jsx
@@ -30,10 +30,14 @@ import Table from './pages/Table.jsx';
 import AdminRoutes from './components/AdminRoutes.jsx';
 import Download from './components/Download.jsx';
 import DateRangeSelector from './components/DateRangeSelector.jsx';
+
+import Landing from './pages/Landing.jsx';
+
 import RestPassword from './pages/auth/RestPassword.jsx';
 import ForgotPassword from './pages/auth/ForgotPassword.jsx';
 import ConfirmAccount from './pages/auth/ConfirmAccount.jsx';
 // import Feedbacks from './components/Feedbacks.jsx';
+
 
 const router = createBrowserRouter(
   createRoutesFromElements(
@@ -84,7 +88,10 @@ const router = createBrowserRouter(
       <Route path='/forgotpassword/' element={<ForgotPassword />} />
       <Route path='/password-reset/:token' element={<RestPassword />} />
       <Route path='/profile' element={<Profile />} />
+
+      
       {/* <Route path='/feedback/:jobId' element={<Feedbacks />} /> */}
+
 
       <Route path='*' element={<NotFound />}></Route>
     </Route>
