d915f0218017a7e440cba6f39b4b96d7f8dd1e4d
kamlesh <k24suthar@gmail.com>
2024-10-25 00:20:26 +0530

    added bg blur and name changes

--- src/components/RegisterForm.jsx
+++ src/components/RegisterForm.jsx
@@ -11,7 +11,7 @@ const RegisterForm = ({ job }) => {
             // alert('Please login to register for the event')
             toast('Please login to register for the event');
         }
-        else{
+        else {
             setModal(!modal);
         }
     }
@@ -33,7 +33,7 @@ const RegisterForm = ({ job }) => {
         if (response.ok) {
             const data = await response.json();
             toast.success(data.message);
-        } 
+        }
         else {
             console.log(response)
             toast.error('Unexpected error occurred');
@@ -62,7 +62,7 @@ const RegisterForm = ({ job }) => {
                 ))}
             </div> */}
 
-            {   !isAuthenticated ?
+            {!isAuthenticated ?
 
                 <a href="/login"
                     className="bg-indigo-500 hover:bg-indigo-600 text-white text-center font-bold py-2 px-4 rounded-full w-full focus:outline-none focus:shadow-outline mt-4 block"
@@ -79,7 +79,7 @@ const RegisterForm = ({ job }) => {
             }
 
             {/* <!-- Main modal --> */}
-            <div id="default-modal" tabIndex="-1" aria-hidden="true" className={`${modal ? '' : 'hidden'} flex overflow-y-auto overflow-x-hidden fixed top-0 right-0 left-0 z-50 justify-center items-center w-full md:inset-0 h-[calc(100%-1rem)] max-h-full`}>
+            <div id="default-modal" tabIndex="-1" aria-hidden="true" className={`${modal ? '' : 'hidden'} flex overflow-y-auto overflow-x-hidden fixed top-0 right-0 left-0 z-50 justify-center items-center w-full md:inset-0 h-[calc(100%-1rem)] max-h-full backdrop-blur-sm`}>
                 <div className="relative p-4 w-full max-w-2xl max-h-full">
                     {/* <!-- Modal content --> */}
                     <div className="relative bg-white rounded-lg shadow ">
@@ -88,7 +88,7 @@ const RegisterForm = ({ job }) => {
                             {/* <!-- Modal header --> */}
                             <div className="flex items-center justify-between p-4 md:p-5 border-b rounded-t ">
                                 <h3 className="text-xl font-semibold text-gray-900 ">
-                                    Form Fields
+                                    Registration details
                                 </h3>
                                 <button type="button" onClick={togle} className="text-gray-400 bg-transparent hover:bg-gray-200 hover:text-gray-900 rounded-lg text-sm w-8 h-8 ms-auto inline-flex justify-center items-center " data-modal-hide="default-modal">
                                     <svg className="w-3 h-3" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 14 14">
@@ -118,7 +118,7 @@ const RegisterForm = ({ job }) => {
 
                             {/* <!-- Modal footer --> */}
                             <div className="flex items-center p-4 md:p-5 border-t border-gray-200 rounded-b ">
-                                <button type="submit" className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center ">I accept</button>
+                                <button type="submit" className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center ">Accept</button>
                                 <button onClick={togle} type="button" className="py-2.5 px-5 ms-3 text-sm font-medium text-gray-900 focus:outline-none bg-white rounded-lg border border-gray-200 hover:bg-gray-100 hover:text-blue-700 focus:z-10 focus:ring-4 focus:ring-gray-100 ">Decline</button>
                             </div>
                         </form>
