c6ec14a3a4aa1ad7e0af2afe911a3e699625cd3e
kamlesh <k24suthar@gmail.com>
2024-10-24 22:38:06 +0530

    grid layout setup

--- src/components/HomePagePastEvents.jsx
+++ src/components/HomePagePastEvents.jsx
@@ -43,12 +43,12 @@ const HomePagePastEvents = () => {
     }
     else if (error) return <div className="text-red-500 text-center">{error}</div>;
     return (
-        <section className="w-full py-12 md:px-36">
-            <div className="container mx-auto px-4 md:px-6">
+        <section className="bg-blue-50 px-4 py-10">
+            <div className="container-xl lg:container m-auto">
                 <h2 className="text-3xl font-bold text-indigo-500 mb-6 text-center">
                     Past Events
                 </h2>
-                <div className="grid gap-6 md:grid-cols-2 lg:grid-cols-3">
+                <div className="grid grid-cols-1 md:grid-cols-3 gap-6">
                     {recent.map((event, i) => {
                         // Calculate days ago
 
