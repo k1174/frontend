3eba118e82fad539863913c35e039f248c59756c
Baljit Singh <baljitsingh692002@gmail.com>
2024-10-26 10:16:41 +0530

    updated and added popupmodal and download component and jobpage (#18)

--- src/pages/JobPage.jsx
+++ src/pages/JobPage.jsx
@@ -7,12 +7,19 @@ import { useAuth } from "../../context/AuthContext";
 import TotalRegistration from "../components/TotalRegistration";
 // import DownloadReportButton from "../components/Report";
 // import Feedbacks from "../components/Feedbacks.jsx";
+import PopupModal from "../components/PopupModal"
 const JobPage = () => {
     const job = useLoaderData();
     const { isAdmin, isAuthenticated } = useAuth();
     const url = '/dashboard/' + job._id;
 
     const [creator, setCreator] = useState(false)
+
+    
+    const [showPopupModal, setShowPopupModal] = useState(false);
+// 
+
+
 
 
     useEffect(() => {
@@ -40,7 +47,17 @@ const JobPage = () => {
         if (isAuthenticated) {
             fetchData()
         }
-    }, [job])
+
+        if (showPopupModal) {
+            document.body.style.overflow = 'hidden';
+        } else {
+            document.body.style.overflow = 'auto';
+        }
+    
+        return () => {
+            document.body.style.overflow = 'auto';
+        }
+    }, [job, showPopupModal])
 
     return (
         <>
@@ -53,7 +70,10 @@ const JobPage = () => {
                     <div className="flex justify-center">
                         {/* <DownloadEmailsButton job={job} /> */}
                         {/* <DownloadReportButton job={job} /> */}
-                        <a href={url} className="text-blue-700 hover:text-white border border-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center me-2 mb-2" >DashBoard</a>
+                        {/* <a href={url} className="text-blue-700 hover:text-white border border-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center me-2 mb-2" >DashBoard</a> */}
+                        <a  className="hover:cursor-pointer" onClick={() => { setShowPopupModal(true) }} >Dashboard</a>
+                        {showPopupModal && <PopupModal onClose={() => setShowPopupModal(false)} />}
+
                         {/* <Feedbacks job={job} /> */}
                     </div>
 
