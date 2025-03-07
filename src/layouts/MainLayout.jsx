a34349921d26cfc99d42f640364b0287b8e67925
Baljit Singh <baljitsingh692002@gmail.com>
2025-03-07 15:21:17 +0530

    Breadcum Feature (#19)
    * updated and added popupmodal and download component and jobpage
    
    * Feature: Added the Breadcum in the Main Layout of the Application
    
    * Removed src/download.jsx from Git tracking
    
    * Feature: Added the breadcum feature in the application

--- src/layouts/MainLayout.jsx
+++ src/layouts/MainLayout.jsx
@@ -3,11 +3,14 @@ import { Outlet } from "react-router-dom";
 import { ToastContainer } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
 import Footer from "../components/Footer";
+import BackLink from "../components/BackLink";
 const MainLayout = () => {
     return (
         <div className="relative min-h-screen flex flex-col max-w-full overflow-hidden ">
             <Navbar />
             <div className="flex-grow">
+            <BackLink />
+
                 <Outlet />
             </div>
             < Footer />
